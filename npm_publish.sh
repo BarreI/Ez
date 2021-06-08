f73d68597ef5ecfc3a2e32a4fedf951742b70e82
lp0ql <59675400+lp0ql@users.noreply.github.com>
2021-06-08 14:29:29 +0900

    npmパッケージ化出来た

---
+++ npm_publish.sh
@@ -0,0 +1,34 @@
+clear
+echo "npm での公開処理"
+echo " "
+cat ~/.npmrc
+
+echo -n "ここに公開しても良いですか?"
+read ANS
+
+case $ANS in
+    [Yy]*)
+    clear
+    
+    npm pack --dry-run
+    
+    echo -n "このファイルを公開しても良いですか?"
+    read ANS_2
+
+    case $ANS_2 in 
+    [Yy]*)
+        sh ready.sh
+        clear
+        echo "公開中..."
+        npm publish
+    ;;
+    *)
+    clear
+    echo "公開をキャンセルしました。"
+    esac
+
+    ;;
+    *)
+    clear
+    echo "公開をキャンセルしました。"
+esac
